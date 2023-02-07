6fb235f6645b4811696d66066fadb335604b4abc
karmpatel960 <96860884+Karmpatel960@users.noreply.github.com>
2023-02-07 18:23:00 +0530

    login page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,86 @@
-import React from "react";
-
-import { Refine } from "@pankod/refine-core";
+import { Refine, AuthProvider } from "@pankod/refine-core";
 import {
-  notificationProvider,
-  Layout,
-  ReadyPage,
-  ErrorComponent,
+    notificationProvider,
+    Layout,
+    ErrorComponent,
 } from "@pankod/refine-antd";
-import "@pankod/refine-antd/dist/reset.css";
-
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router-v6";
 
-function App() {
-  return (
-    <Refine
-      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-      notificationProvider={notificationProvider}
-      Layout={Layout}
-      ReadyPage={ReadyPage}
-      catchAll={<ErrorComponent />}
-      routerProvider={routerProvider}
-    />
-  );
-}
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+
+import "@pankod/refine-antd/dist/reset.css";
+
+import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
+import { Login } from "pages/Login";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+const App: React.FC = () => {
+    const { isLoading, user, logout, getIdTokenClaims } = useAuth0();
+
+    if (isLoading) {
+        return <span>loading...</span>;
+    }
+
+    const authProvider: AuthProvider = {
+        login: () => {
+            return Promise.resolve(false);
+        },
+        logout: () => {
+            logout({ returnTo: window.location.origin });
+            return Promise.resolve("/");
+        },
+        checkError: () => Promise.resolve(),
+        checkAuth: async () => {
+            try {
+                const token = await getIdTokenClaims();
+                if (token) {
+                    axios.defaults.headers.common = {
+                        Authorization: `Bearer ${token.__raw}`,
+                    };
+                    return Promise.resolve();
+                } else {
+                    return Promise.reject();
+                }
+            } catch (error) {
+                return Promise.reject();
+            }
+        },
+        getPermissions: () => Promise.resolve(),
+        getUserIdentity: async () => {
+            if (user) {
+                return Promise.resolve({
+                    ...user,
+                    avatar: user.picture,
+                });
+            }
+            return Promise.reject();
+        },
+    };
+
+    return (
+        <Refine
+            LoginPage={Login}
+            authProvider={authProvider}
+            dataProvider={dataProvider(API_URL, axios)}
+            routerProvider={routerProvider}
+            resources={[
+                {
+                    name: "posts",
+                    list: PostList,
+                    create: PostCreate,
+                    edit: PostEdit,
+                    show: PostShow,
+                },
+            ]}
+            notificationProvider={notificationProvider}
+            Layout={Layout}
+            catchAll={<ErrorComponent />}
+        />
+    );
+};
 
 export default App;
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
@@ -8,9 +10,15 @@ const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Auth0Provider
+            domain="refine.eu.auth0.com"
+            clientId="zHwgQ2SoYUDQo3Ng1Bdtyk5eGoa2ad7X"
+            redirectUri={window.location.origin}
+        >
+            <App />
+        </Auth0Provider>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
